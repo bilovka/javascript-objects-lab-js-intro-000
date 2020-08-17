4d7a0354c825575e6c711e954a922a05cff6e7d5
Oleksandr <bilovka10@gmail.com>
2020-08-17 16:26:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = new Object({});
+function updateObjectWithKeyAndValue(object, key, value){
+  console.log( recipes[key]=value);
+  return Object.assign({},object, {[key]:value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){ 
+  object[key]=value;
+  return object;
+}
