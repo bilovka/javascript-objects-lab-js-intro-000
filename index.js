eda84cbc487779ced4d1016db16c715a54ebfc73
Oleksandr <bilovka10@gmail.com>
2020-08-17 16:44:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromObjectByKey(object, key){
 }
 function destructivelyDeleteFromObjectByKey(object, key){
   object.key=['zz'];
-  delete recipes.key;
-  return recipes;
+  delete object.key;
+  return object;
 }
